feac9228b0331bf6c0019e2165cde8b000b9a7e0
Casey Gower <caseyg@charter.net>
2018-05-08 23:24:18 -0500

    Add Sign Up Page

--- MissionConnect/wwwroot/js/site.js
+++ MissionConnect/wwwroot/js/site.js
@@ -1,12 +1,14 @@
-﻿// Write your Javascript code.
-function addImage(pk) {
-    alert("addImage: " + pk);
-}
+﻿// Get the container element
+var btnContainer = document.getElementById("myModal");
 
-$('#myModal .save').click(function (e) {
-    e.preventDefault();
-    addImage(5);
-    $('#myModal').modal('hide');
-    //$(this).tab('show')
-    return false;
-})
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav-link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
