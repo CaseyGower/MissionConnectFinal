a55b715de9c0030b0af22e35137f8f14fb521c7c
Casey Gower <caseyg@charter.net>
2018-05-08 21:52:30 -0500

    Working Modal (without CSS and JS)

--- MissionConnect/wwwroot/js/site.js
+++ MissionConnect/wwwroot/js/site.js
@@ -1,1 +1,12 @@
 ﻿// Write your Javascript code.
+function addImage(pk) {
+    alert("addImage: " + pk);
+}
+
+$('#myModal .save').click(function (e) {
+    e.preventDefault();
+    addImage(5);
+    $('#myModal').modal('hide');
+    //$(this).tab('show')
+    return false;
+})
