466a99e04fd2af4776595aedd383d8b0b66776bf
Casey Gower <caseyg@charter.net>
2018-05-08 20:28:28 -0500

    Add Mission Connect

---
+++ MissionConnect/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace MissionConnect.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public object Image {get;set;}
+        public object Location {get;set;}
+        public string Status {get;set;}
+        public string ReligiousAffiliation {get;set;}
+        public string LocationOfInterest {get;set;}
+        public string MissionBio {get;set;}
+
+    }
+}
---
+++ MissionConnect/wwwroot/js/discover.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    $.getJSON('/js/missionconnect-data.json', function (data) {
+        console.log(data);
+        var fhu = new google.maps.LatLng(39.0072516,-94.5316662);
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 2,
+          center: fhu
+        });
+
+        for( var i=0; i < data.CurrentMissionaries.length; i++) {
+            var m = data.CurrentMissionaries[i];
+            var latLng = new google.maps.LatLng( m.Lat, m.Long);
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map
+            });
+        }
+      });
+
+});
